0202237375886e1b35f8bef7a0b21c12b2143317
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2023-12-06 20:25:52 +0000

    fluid grid bootstrap background style updated

--- client/src/components/ArtIndex.jsx
+++ client/src/components/ArtIndex.jsx
@@ -13,13 +13,13 @@ export default function ArtIndex() {
   const [arts, setArts] = useState([])
   const [artistChoice, setArtistChoice] = useState('All Artists')
 
-  
-  
+
+
   //! Effects
   useEffect(() => {
     async function getArtData() {
       try {
-        const res  = await axios.get('/api/art')
+        const res = await axios.get('/api/art')
         console.log(res)
         console.log(res.data)
         setArts(res.data)
@@ -30,7 +30,7 @@ export default function ArtIndex() {
     }
     getArtData()
   }, [])
-  
+
   const artistAll = [... new Set(arts.map(art => art.artist))]
 
 
@@ -46,17 +46,17 @@ export default function ArtIndex() {
     <>
       <section className="filter-container">
         {/* <form onSubmit={handleSubmit}>   */}
-          <select
-            className="artist-list"
-            onChange={(e) => setArtistChoice(e.target.value)}
-            value={artistChoice}
-          >
-            {artistAll
-              .map((artistChoice, i) => {
-                return <option key={i} value={artistChoice}>{artistChoice}</option>
-              })
-            }
-          </select>
+        <select
+          className="artist-list"
+          onChange={(e) => setArtistChoice(e.target.value)}
+          value={artistChoice}
+        >
+          {artistAll
+            .map((artistChoice, i) => {
+              return <option key={i} value={artistChoice}>{artistChoice}</option>
+            })
+          }
+        </select>
         {/* </form> */}
       </section>
 
@@ -65,31 +65,33 @@ export default function ArtIndex() {
         <Container fluid>
           <Row className="artsAll-list">
             {arts
-              .map(art => {
-                const { id, artName, artImage} = art
+              .map((art, i) => {
+                const { id, artName, artImage } = art
                 return (
                   <Col
                     as={Link}
-                    key={id}
+                    key={i}
                     xs={4}
                     s={3}
                     md={3}
                     lg={2}
                     xl={2}
+                    style={{ backgroundImage: `url(${artImage})`, backgroundRepeat:'no-repeat' , backgroundPosition:'center', backgroundSize:'contain'}}
                     to={`/art/${id}`}
                   >
-                    <div className="rails">
-                      <img className="thumbnail" src={artImage} to={`/art/${id}`} />
-                      <div className="art-title">
-                        <p>{artName}</p>
-                      </div>
+                    {/* {artName} */}
+                    <div className="rails" style={{height: '300px'}}>
+                    {/* <img className="thumbnail" src={artImage} to={`/art/${id}`} /> */}
+                    <div className="art-title">
+                      <p>{artName}</p>
                     </div>
+                  </div>
                   </Col>
-                )
+          )
               })}
-          </Row>
-        </Container>
-      </main >
+        </Row>
+      </Container>
+    </main >
     </>
   )
 }
