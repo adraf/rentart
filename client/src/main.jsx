f7e0bfe56eb69c5a68ba2b311a532bed432b6785
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2023-12-06 10:47:51 +0000

    last commit before moving in correct branches

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
+//! Styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.scss'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import router from './config/routes.js'
+
+//! Variables
+const app = express()
+const port = process.env.PORT
+const connectionString = process.env.CONNECTION_STRING
+
+//! Middleware 
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(`REQUEST RECEIVED: ${req.method} ${req.originalUrl}`)
+  next()
+})
+
+//! Endpoints
+app.use('/api', router)
+
+// Start servers
+async function startServer() {
+  try {
+    // Establish a connection to the MongoDB database (Atlas)
+    await mongoose.connect(connectionString)
+    console.log('✅ Database connection established')
+    // Listen
+    app.listen(port, () => console.log(`🚀 Server listening on port ${port}`))
+  } catch (error) {
+    console.log('🚨 Error connection')
+    console.log(error)
+  }
+}
+startServer()
