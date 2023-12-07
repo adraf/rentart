d0ae8b61ffc00cea0c78c73bbab0193cce697f14
Antonio <acliment86@gmail.com>
2023-12-07 14:04:00 +0100

    Register form created, given basic styling and started the server side authentication

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -14,10 +14,14 @@ export default function Nav(){
       <Link to='/gallery'><button>Gallery</button></Link>
       <Link to='/about'><button>About us</button></Link>
       <Link to='/collection/'><button>Collection</button></Link>
-      {!userData
-      ? <Link to='/login/'><button>Login</button></Link> 
-      : <Link to={`/profile/${userData.id}`}><button>Profile</button></Link>
-      }
+      {!userData ? (
+        <>
+          <Link to='/login/'><button>Login</button></Link>
+          <Link to='/register'><button>Register</button></Link>
+        </>
+      ) : (
+        <Link to={`/profile/${userData.id}`}><button>Profile</button></Link>
+      )}
     </nav>
   )
 }
