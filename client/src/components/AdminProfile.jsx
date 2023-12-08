b88153510dbe939eeb8788fe692b664863944c10
Antonio <acliment86@gmail.com>
2023-12-08 20:05:48 +0100

    created a new router not yet debugged, coded most of adminprofile, not yet debugged

--- client/src/components/AdminProfile.jsx
+++ client/src/components/AdminProfile.jsx
@@ -1,13 +1,30 @@
 import { useState } from "react"
+import axios from "axios"
 
-export default function AdminProfile(){
+export default function AdminProfile({ userData, setUserData }){
 
   const [searchData, setSearchData] = useState('')
 
   async function search(e){
-    e.preventDefault()
-    const inputValue = e.target.elements.searchInput.value
+    try {
+      e.preventDefault()
+      const inputValue = e.target.elements.searchField.value
+      const pattern = new RegExp(inputValue, 'i')
+      const res = await axios.get('/api/art')
+      const rawArtData = res.data
+      const parsedArtData = rawArtData.filter(piece => pattern.test(piece.artName))
+      // ! Fix the calls!
+      const response = await axios.get('/api/profile/all', { headers: { Authorization: `Bearer ${ userData.token }` } })
+      const rawUserData = response.data
+      console.log(rawUserData)
+      const parsedUserData = rawUserData.filter(user => pattern.test(user.username))
+      setSearchData({ userData: parsedUserData, artData: parsedArtData })
+    } catch (error) {
+      console.log(error)
+    }
   }
+
+
 
   return (
     <section className="profile">
@@ -16,19 +33,44 @@ export default function AdminProfile(){
       </div>
       <div className="profile-body admin-body">
         <form onSubmit={search} className="admin-search">
-          <input type="text" placeholder="Search by username, name of artwork" className="admin-search" />
-          {
+          <input type="text" name="searchField" placeholder="Search by username, name of artwork" className="admin-search" />
+        </form>
+        {
           !searchData
           ?
           <h2 className="negative-results">No results...</h2>
           :
-          searchData.map(result => {
-            <section className="admin-result">
-              <p>placeholder</p>
+          <section className="admin-result">
+          {searchData.userData.map(user => {
+            <section className="single-user-result" key={user._id}>
+              {
+              !user.profileImage
+              ?
+              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+              :
+              <img src={user.profileImage} alt="" />
+              }
+              <div className="text-data">
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Address: {user.address}</p>
+                <p>Payment Details: {user.paymentDetails}</p>
+              </div>
             </section>
-          })
-          }
-        </form>
+          })}
+          {searchData.artData.map(art => {
+            <section className="single-art-result" key={art._id}>
+              <img src={art.artImage} alt="" />
+              <div className="text-data">
+                <p>Artist: {art.artist}</p>
+                <p>Name: {art.artName}</p>
+                <p>Year: {art.year}</p>
+                <p>Movement: {art.movement}</p>
+              </div>
+            </section>
+          })}
+          </section>
+        }
         <div className="admin-results"></div>
       </div>
     </section>
