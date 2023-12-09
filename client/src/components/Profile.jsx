e15d0f8b219877ae8102110aee5a530cea8d5f2e
adraf <adraf@me.com>
2023-12-09 16:22:46 +0000

    continued with image uploader, will need profile's finished to see where it fits in. It uploads to cloudinary.

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,18 +4,11 @@ import { useState } from 'react'
 import ArtistProfile from "./ArtistProfile"
 import AppreciatorProfile from "./AppreciatorProfile"
 import AdminProfile from "./AdminProfile"
-import ImageUploadField from './ImageUploadField'
 
 export default function Profile(){
   // State
   const data = useOutletContext()
   const [ userData, setUserData ] = data
-
-  // image uploader
-  // const [ formData, setFormData ] = useState({profileImage: ''})
-  // function handleChange(e) {
-  //   setFormData({ ...formData, [e.target.name]: e.target.value })
-  // }
 
   // User type variable to render appropriate component
   const type = localStorage.getItem('usertype')
@@ -32,10 +25,6 @@ export default function Profile(){
       : type === '2' ? <AppreciatorProfile />
       : type === '0' && <AdminProfile />
       }
-      {/* <form onSubmit={e => e.preventDefault()}>
-        <ImageUploadField setFormData={setFormData} formData={formData}/>
-        <input type="submit" value="Add Profile Picture" onSubmit={handleChange} />
-      </form> */}
     </>
   )
 }
