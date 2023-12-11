ff248c54a20b5f7fab21e0ac69864612eae4fac7
Anne-Laure Guiot <annelaure.guiot@gmail.com>
2023-12-11 15:00:22 +0000

    public page getting user info

--- client/src/components/PublicProfile.jsx
+++ client/src/components/PublicProfile.jsx
@@ -1,6 +1,6 @@
-// import { Link } from 'react-router-dom'
-
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
+// import axios from 'axios'
+// import { useEffect } from 'react'
 
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -8,6 +8,19 @@ import Col from 'react-bootstrap/Col'
 
 
 export default function IndUserPage() {
+
+  // // //! Effects
+  // useEffect(() => {
+  //   async function getArtData() {
+  //     try {
+  //       const res = await axios.get('/api/art')
+  //       setArts(res.data)
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
+  //   getArtData()
+  // }, [])
 
   const user = useLoaderData()
 
@@ -20,14 +33,20 @@ export default function IndUserPage() {
     rented,
     personal_collection } = user
 
-    console.log(user.username)
 
   return (
     <main>
-      <Container className='indArtContainer' fluid={true}>
-        <Row className='indArtSection'>
-          <Col className='indArtImageColumn' sm={5}><img src={profileImage} alt={username} /></Col>
-          <Col className='indArtTextColumn'>
+      <Container className='' fluid={true}>
+        <Row className=''>
+          <Col className='' sm={2}>
+            {
+              !user.profileImage
+                ?
+                <img className="search-image" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+                :
+                <img className="search-image" src={profileImage} alt={username} />
+            }</Col>
+          <Col className=''>
             <Row>
               <h2>Name: {name}</h2>
               <h4>Username: {username}, {usertype}</h4>
@@ -37,12 +56,46 @@ export default function IndUserPage() {
               <Col></Col>
             </Row>
             <Row>
-              <Col>Favourites:</Col>
-              <Col>{favourites}</Col>
-            </Row>
-            <Row>
               <Col>Currently on rent:</Col>
               <Col>{rented}</Col>
+            </Row>
+            <Row>
+              <Col>Favourites:</Col>
+              <Col>
+              <Container>
+                <Row>
+                  {favourites
+                    .map((art, i) => {
+                      const { _id: indArtId, artName, artImage, artist } = art
+                      return (
+                        <Col
+                          className=''
+                          // Link helps the individual art page function
+                          as={Link}
+                          key={i}
+                          xs={12}
+                          s={12}
+                          md={6}
+                          lg={6}
+                          xl={4}
+                          style={{ backgroundImage: `url(${artImage})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'contain' }}
+                          to={`/art/${indArtId}`}
+                        >
+                          <div className="rails" style={{ height: '300px' }}>
+                            <div className="thumbnail" to={`/art/${indArtId}`}
+                              style={{ backgroundImage: `url(${artImage})` }}>
+                            </div>
+                            <div className="art-title">
+                              <h5>art{artName}</h5>
+                              <p>artist {artist}</p>
+                            </div>
+                          </div>
+                        </Col>
+                      )
+                    })}
+                </Row>
+              </Container>
+              </Col>
             </Row>
             <Row>
               <Col>Artist Collection:</Col>
